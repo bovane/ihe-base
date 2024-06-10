8d1cea22493686a495b52253f1f94e1bff5f8bb8
bovane <40169572+bovane@users.noreply.github.com>
2024-06-10 17:20:17 +0800

    备份测试

--- ihe-xds/src/main/java/work/mediway/ihe/Main.java
+++ ihe-xds/src/main/java/work/mediway/ihe/Main.java
@@ -1,11 +1,19 @@
 package work.mediway.ihe;
 
+import org.openehealth.ipf.commons.audit.model.AuditMessage;
+import org.openehealth.ipf.commons.audit.queue.RecordingAuditMessageQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * @author bovane bovane.ch@gmial.com
  * @create 2024/6/7
  */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
