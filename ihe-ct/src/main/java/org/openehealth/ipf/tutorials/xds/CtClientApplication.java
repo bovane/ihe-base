4bb5f9751426080a83e15d2e22cca6611e9d9818
bovane <40169572+bovane@users.noreply.github.com>
2024-06-19 16:12:34 +0800

    完善IHE CT 相关代码

---
+++ ihe-ct/src/main/java/org/openehealth/ipf/tutorials/xds/CtClientApplication.java
@@ -0,0 +1,15 @@
+package org.openehealth.ipf.tutorials.xds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CtClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtClientApplication.class, args);
+    }
+
+}
