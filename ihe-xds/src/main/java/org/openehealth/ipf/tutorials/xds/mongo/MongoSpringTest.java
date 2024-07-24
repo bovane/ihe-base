21253830f01aad949a4512ec309b6c54c765e72b
bovane <40169572+bovane@users.noreply.github.com>
2024-07-24 16:51:09 +0800

    完善ITI41创建文档的Helper

---
+++ ihe-xds/src/main/java/org/openehealth/ipf/tutorials/xds/mongo/MongoSpringTest.java
@@ -0,0 +1,12 @@
+package org.openehealth.ipf.tutorials.xds.mongo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author bovane bovane.ch@gmial.com
+ * @create 2024/7/24
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongoSpringTest {
+}
---
+++ ihe-xds/src/main/java/org/openehealth/ipf/tutorials/xds/mongo/MongoTest.java
@@ -0,0 +1,34 @@
+package org.openehealth.ipf.tutorials.xds.mongo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.internal.MongoClientImpl;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author bovane bovane.ch@gmial.com
+ * @create 2024/7/24
+ */
+@Slf4j
+public class MongoTest {
+
+    public static void main(String[] args) {
+        // 创建MongoDB客户端
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        // 创建数据库
+        MongoDatabase database = mongoClient.getDatabase("ihe-base");
+
+        // 获取所有数据库名称
+        MongoIterable<String> databaseNames = mongoClient.listDatabaseNames();
+
+        System.out.println("数据库名称:");
+        for (String dbName : databaseNames) {
+            System.out.println(dbName);
+        }
+
+        // 关闭MongoDB客户端
+        mongoClient.close();
+    }
+}
